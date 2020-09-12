7d59bedcec96bfa5eab5597906b0b176fdf3f092
Erin Oefelein <oefeleinerin@gmail.com>
2020-09-11 20:34:15 -0500

    Revert "submitting HW_01"
    This reverts commit 7d3fe03431d271877a728fd358b37265d7b249fb.

--- test/classwork_test/01_assign_test/types_tests.cpp
+++ test/classwork_test/01_assign_test/types_tests.cpp
@@ -1,9 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "types.h"
-#include <string>
-
-using namespace std;
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -12,10 +9,4 @@ TEST_CASE("Verify Test Configuration", "verification") {
 TEST_CASE("Test sum_numbers function") 
 {
 	REQUIRE(sum_numbers(5, 5) == 10);
-}
-
-TEST_CASE("Test Multiply Numbers") 
-{
-	REQUIRE(multiply_numbers(10) == 50);
-	REQUIRE(multiply_numbers(2) == 10);
 }
