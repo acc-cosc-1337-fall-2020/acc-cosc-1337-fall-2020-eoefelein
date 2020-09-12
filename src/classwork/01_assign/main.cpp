7d3fe03431d271877a728fd358b37265d7b249fb
Erin Oefelein <oefeleinerin@gmail.com>
2020-09-11 20:28:12 -0500

    submitting HW_01

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -1,12 +1,24 @@
 //Write the include statement for types.h here
-
+#include "types.h"
 //Write include for capturing input from keyboard and displaying output to screen
+#include <iostream>
 
-
+using namespace std;
 
 int main() 
 {
+	int num;
+	
+	cout << "Enter a number: \n";
+	cin >> num;
 
+	int result = multiply_numbers(num);
+	cout << "Result=" << result << endl;
+
+	int num1 = 4;
+
+	result = multiply_numbers(num1);
+	cout << "Result=" << result << endl;
+	
 	return 0;
 }
-
--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -1,15 +1,16 @@
 ﻿//write include statements
-
+#include <iostream>
 
 //write namespace using statement for cout
-
+using namespace std;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
-
+	int number;
+	cin >> number;
+	cout << number;
 	return 0;
 }
