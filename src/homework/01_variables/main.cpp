fd5b6db4d6e2cc5e17734ddda048dc5393678b7c
Erin Oefelein <oefeleinerin@gmail.com>
2020-09-16 19:25:03 -0500

    "homework_01"

--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -1,7 +1,8 @@
 ﻿//write include statements
 #include <iostream>
+#include "variables.h"
 
-//write namespace using statement for cout
+//write namespace using statement for cin, cout
 using namespace std;
 
 /*
@@ -9,8 +10,25 @@ Call multiply_numbers with 10 and 10 parameter values and display function resul
 */
 int main()
 {
-	int number;
-	cin >> number;
-	cout << number;
+	double meal_amount;
+	double tip_rate;
+	double tip_amount;
+	double tax_amount;
+	double total;
+
+	cout << "Enter the meal amount: ";
+	cin >> meal_amount;
+	tax_amount = get_sales_tax_amount(meal_amount);
+
+	cout << "Enter the tip rate: ";
+	cin >> tip_rate;
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+	total = tip_amount + tax_amount + meal_amount;
+
+	cout << "Meal Amount: 	" << meal_amount << endl;
+	cout << "Sales Tax: 	" << tax_amount << endl;
+	cout << "Tip: 		" << tip_rate << endl;
+	cout << "Total: 		" << total;	
 	return 0;
 }
--- src/homework/01_variables/variables.cpp
+++ src/homework/01_variables/variables.cpp
@@ -6,9 +6,17 @@ int add_numbers(int num1, int num2)
 	return num1 + num2;
 }
 
-//write code for multiply_numbers function here
-//function returns product of num1 and num2
+//get_sales_tax_amount function
+//function returns product of meal_amount and tax_rate
+const double tax_rate = 0.0675;
+double get_sales_tax_amount(double meal_amount)
+{
+	return tax_rate * meal_amount;
+}
 
-
-
-
+//get_sales_tax_amount function
+//function returns prduct of meal_amount and tip_rate
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+	return meal_amount * tip_rate;
+}
