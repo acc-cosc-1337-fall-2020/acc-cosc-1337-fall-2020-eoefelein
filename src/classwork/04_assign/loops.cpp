f2327944c1ed624db4b8e2eefa89c6e70f938aea
Erin Oefelein <oefeleinerin@gmail.com>
2020-10-02 18:32:10 -0500

    Homework 03 - loops

--- src/classwork/04_assign/loops.cpp
+++ src/classwork/04_assign/loops.cpp
@@ -1,1 +1,16 @@
 //cpp
+
+//Write the include statement for loops.h
+#include "loops.h"
+
+//factorial
+int factorial(int num){
+    int total=1;
+    int factorial=1;
+
+    while(factorial<num+1){
+        total=total*factorial;
+        factorial++;
+    }
+    return total;
+}
--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -1,7 +1,9 @@
 //write includes statements
-
+#include <iostream>
+#include "loops.h"
 //write using statements for cin and cout
-
+using namespace std;
+// using std::cout; using std::cin;
 
 /*
 Use a do while loop to prompt the user for 
@@ -10,6 +12,31 @@ factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
 {
+	char choice;
 
+	do{
+		cout<<"Enter a number between 1 and 10: "<<endl;
+		int num;
+		cin>>num;
+		cout<<"\n";
+
+		while(!(num >=1 && num <=10))
+		{
+			cout<< "Number is invalid" << endl;
+
+			cout<<"Enter a number between 1 and 10: ";
+			cout<<"\n";
+			cin>>num;
+
+		}
+		int result = factorial(num);
+		cout << "Result=" << result << endl;
+
+		// check to make sure this is how char var would get defined
+		cout<<"\nEnter y if you wish to continue. Enter n to exit."<<endl;
+		cin>>choice;
+ 
+	}while(choice != 'n');
+	
 	return 0;
 }
