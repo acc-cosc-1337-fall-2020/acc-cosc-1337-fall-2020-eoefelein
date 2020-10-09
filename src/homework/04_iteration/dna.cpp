c368c418038b99c30bdaef2dba54779764679fa8
Erin Oefelein <oefeleinerin@gmail.com>
2020-10-09 18:24:06 -0500

    Homework 4 - DNA iteration

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -1,4 +1,8 @@
 #include "dna.h"
+#include<string>
+#include<iostream>
+
+using std::string; using std::cout;
 /*
 Write code for function get_gc_content that accepts
 a const reference string parameter and returns a double.
@@ -6,15 +10,33 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
-
-
-
+double get_gc_content(const std::string& dna){
+    // declare accumulator
+    double gc = 0;
+    for (int i=0; i<dna.length(); i++)
+    {
+        if( dna[i] == 'G' || dna[i] == 'C')
+        {
+            gc++;
+        }
+    }
+    return gc/dna.length();
+}
 
 /*
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
-
+// may have to start from the beginning but test this first...
+std::string get_reverse_string(std::string dna)
+{
+    string revDNA = "";
+    for(int i=dna.length()-1; i>=0; i--)
+    {
+        revDNA.push_back(dna[i]);
+    }
+    return revDNA;
+}
 
 
 /*
@@ -27,4 +49,26 @@ b. iterate local string variable and
 c. return string
 
 */
-
+string get_dna_complement(string dna)
+{
+    string rev_str = get_reverse_string(dna);
+    for (int i=0; i<rev_str.length(); i++)
+    {
+        switch(rev_str[i])
+        {
+        case 'A':
+            rev_str[i] = 'T';
+            break;
+        case 'T':
+            rev_str[i] = 'A';
+            break;
+        case 'C':
+            rev_str[i] = 'G';
+            break;
+        case 'G':
+            rev_str[i] = 'C';
+            break;
+        }
+    }
+    return rev_str;
+}
--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -1,7 +1,10 @@
 //write include statements
+#include "dna.h"
+#include<string>
+#include<iostream>
 
 //write using statements
-
+using std::string; using std::cout; using std::cin;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +15,37 @@ user enters a y or Y.
 */
 int main() 
 {
+	string dummy = "";
+
+	do {
+	
+	string DNA;
+	cout<<"Enter your DNA string in capital letters: "<<"\n";
+	cin>>DNA;
+
+	int choice;
+	cout<<"Enter 1 to get DNA GC Content. Enter 2 to get DNA Complement."<<"\n";
+	cin>>choice;
+
+	if (choice == 1)
+	{
+		cout<<get_gc_content(DNA)<<"\n";
+		cout<<"Would you like to input another DNA String? Enter \"y\" or \"Y\" for yes, or any other key to exit the program."<<std::endl;
+		cin>>dummy;
+	}
+	else if (choice == 2)
+	{
+		cout<<get_dna_complement(DNA)<<"\n";
+		cout<<"Would you like to input another DNA String? Enter \"y\" or \"Y\" for yes, or any other key to exit the program."<<std::endl;
+		cin>>dummy;
+	}
+	else 
+	{
+		cout<<"Sorry! Number is invalid. Please enter either a 1 or a 2."<<std::endl;
+		cout<<"Would you like to input another DNA String? Enter \"y\" or \"Y\" for yes, or any other key to exit the program."<<std::endl;
+		cin>>dummy;
+	}
+	} while (dummy == "y" || dummy == "Y");
+	
 	return 0;
 }
